c811878e0f502cc7f741785c154ad90b41334375
Korniichuk Alexandra <angel.win04@gmail.com>
2020-07-10 20:32:43 +0300

    2 lesson

--- hw-1.js
+++ hw-1.js
@@ -25,91 +25,91 @@ if (ordered > total) {
 //task 3
 const ADMIN_PASSWORD = "jqueryismyjam";
 let message;
-const enter = prompt("Enter password please");
+// const enter = prompt("Enter password please");
 // cancel - null
 // ok - jqueryismyjam
 // something else
-if (enter == null) {
-  message = "Отменено пользователем!";
-  const none = alert(message);
-  console.log(none);
-} else if (enter == ADMIN_PASSWORD) {
-  message = "Добро пожаловать!";
-  const welcome = alert(message);
-  console.log(welcome);
-} else {
-  message = "Доступ запрещен, неверный пароль!";
-  const wrong = alert(message);
-  console.log(wrong);
-};
+// if (enter == null) {
+//   message = "Отменено пользователем!";
+//   const none = alert(message);
+//   console.log(none);
+// } else if (enter == ADMIN_PASSWORD) {
+//   message = "Добро пожаловать!";
+//   const welcome = alert(message);
+//   console.log(welcome);
+// } else {
+//   message = "Доступ запрещен, неверный пароль!";
+//   const wrong = alert(message);
+//   console.log(wrong);
+// };
 
 //task 4
 let credits = 23580;
 let pricePerDroid = 3000;
 let message2;
-const quantity = prompt("Какое колчиество роботов вы хотите купить?");
-if (quantity == null) {
-  message2 = "Отменено пользователем!";
-  const none2 = alert(message2);
-  console.log(none2);
-} else if (quantity * pricePerDroid > credits) {
-  message2 = "Недостаточно средств на счету!";
-  const notenoughmoney = alert(message2);
-  console.log(notenoughmoney);
-} else {
-  let droids;
-  droids = credits - pricePerDroid * quantity;
-  message2 = `Вы купили ${quantity} дроидов, на счету осталось ${droids} кредитов.`;
-  const enoughmoney = alert(message2);
-  console.log(enoughmoney);
-};
+// const quantity = prompt("Какое колчиество роботов вы хотите купить?");
+// if (quantity == null) {
+//   message2 = "Отменено пользователем!";
+//   const none2 = alert(message2);
+//   console.log(none2);
+// } else if (quantity * pricePerDroid > credits) {
+//   message2 = "Недостаточно средств на счету!";
+//   const notenoughmoney = alert(message2);
+//   console.log(notenoughmoney);
+// } else {
+//   let droids;
+//   droids = credits - pricePerDroid * quantity;
+//   message2 = `Вы купили ${quantity} дроидов, на счету осталось ${droids} кредитов.`;
+//   const enoughmoney = alert(message2);
+//   console.log(enoughmoney);
+// };
 
 //task 5
 let message3;
 let cost;
-const country = prompt("В какую страну вы хотите доставку?").toLowerCase();
-switch (country) {
-  case "китай":
-    cost = 100;
-    break;
-  case "чили":
-    cost = 250;
-    break;
-  case "австралия":
-    cost = 170;
-    break;
-  case "индия":
-    cost = 80;
-    break;
-  case "ямайка":
-    cost = 120;
-    break;
-  default:
-    message3 = "В вашей стране доставка не доступна";
-    const nocountry = alert(message3);
-    console.log(nocountry);
-    break;
-}
-if (message3 == undefined) {
-  message3 = `Доставка в ${country} будет стоить ${cost} кредитов`;
-  const exit = alert(message3);
-  console.log(exit);
-};
+// const country = prompt("В какую страну вы хотите доставку?").toLowerCase();
+// switch (country) {
+//   case "китай":
+//     cost = 100;
+//     break;
+//   case "чили":
+//     cost = 250;
+//     break;
+//   case "австралия":
+//     cost = 170;
+//     break;
+//   case "индия":
+//     cost = 80;
+//     break;
+//   case "ямайка":
+//     cost = 120;
+//     break;
+//   default:
+//     message3 = "В вашей стране доставка не доступна";
+//     const nocountry = alert(message3);
+//     console.log(nocountry);
+//     break;
+// }
+// if (message3 == undefined) {
+//   message3 = `Доставка в ${country} будет стоить ${cost} кредитов`;
+//   const exit = alert(message3);
+//   console.log(exit);
+// };
 
 //task 6
 let total2 = 0;
 let message4;
 let per;
-let input = prompt("Введите, пожалуйста число");
+let input = +prompt("Введите, пожалуйста число");
 if (input == null) {
     message4 = `Общая сумма чисел равна ${total2}`;
     const sum = alert(message4);
     console.log(sum);
-} else if (Number(input) == NaN) {
+} else if (input == NaN) {
     message4 = "Было введено не число, попробуйте еще раз";
     const nonumber = alert(message4);
     console.log(nonumber);
 } else {
-    total2 = total2 + Number(input);
+    total2 = total2 + input;
     console.log(total2);
 };
