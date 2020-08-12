d5dc514e87ecae91436ed50990e57721842e9e7c
Korniichuk Alexandra <angel.win04@gmail.com>
2020-08-12 14:42:35 +0300

    hw6 done

--- hw/hw6.js
+++ hw/hw6.js
@@ -49,25 +49,30 @@ console.log(calculateTotalBalance(users)); // 20916
 
 //task 8
 const getUsersWithFriend = (users, friendName) => {
-  return users.filter((user) => user.friends.includes(friendName)).map((user) => user.name);
+  return users
+    .filter((user) => user.friends.includes(friendName))
+    .map((user) => user.name);
 };
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
 
 //task 9
 const getNamesSortedByFriendsCount = (users) => {
-  return users.sort((a, b) => a.friends.length - b.friends.length).map((user) => user.name);
+  return users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map((user) => user.name);
 };
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 //task 10
-const getSortedUniqueSkills = users => {
-
+const getSortedUniqueSkills = (users) => {
+  return users.reduce((acc, user) => {
+    user.skills.forEach((oneSkill) =>
+      acc.includes(oneSkill) ? "" : acc.push(oneSkill)
+    );
+    return acc;
+  }, []);
 };
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
-
-
-
-
